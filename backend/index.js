055135a88e790568af5a45680e100c2d2f9cd068
Shivam <shivam.chem21@iitg.ac.in>
2024-08-21 02:13:57 +0530

    Todo app backend done

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/todo", function(req,res){
+app.post("/todo", async function(req,res){
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
@@ -13,13 +13,24 @@ app.post("/todo", function(req,res){
         })
         return;
     }
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "Todo creaated"
+    })
 })
 
-app.get("/todos", function(req,res){
-    
+app.get("/todos", async function(req,res){
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
 })
 
-app.put("/completed", function(req,res){
+app.put("/completed", async function(req,res){
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success){
@@ -28,4 +39,12 @@ app.put("/completed", function(req,res){
         })
         return;
     }
+    await todo.update ({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as completed"
+    })
 })
